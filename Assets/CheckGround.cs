49eaa39115701e3114e13e315679e23a3ec6fa40
crigasro <gasparrozalen@gmail.com>
2019-05-07 20:06:08 +0200

    Momia cambiada

---
+++ Assets/CheckGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    public Mummy player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.GetComponent<Mummy>();
+    }
+
+    void OnTriggerEntered2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        player.grounded = false;
+    }
+
+
+}
---
+++ Assets/PlayerOne.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mummy : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController player;
+
+    public float playerSpeed = 0.1f;
+
+    public Animator animator;
+
+    public bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GetComponent<CharacterController>();
+        playerSpeed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate() 
+    {
+        player.Move(new Vector3(horizontalMove, 0f, verticalMove) * playerSpeed);
+        animator.SetBool("IsMoving", player.velocity.magnitude > 0.1);
+    }
+}
