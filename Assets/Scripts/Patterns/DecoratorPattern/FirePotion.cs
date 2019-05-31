2931e9b3352f6606bd0b219273d8e46b12abc1b2
arnaupool <arnyr@hotmail.es>
2019-06-01 01:16:11 +0200

    Cambios menores

---
+++ Assets/Scripts/Patterns/DecoratorPattern/FirePotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePotion : Potion
+{
+    void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F)) {
+            Debug.Log ("Otorgar poder de fuego al jugador");
+            base.Use();
+        }
+    }
+}
---
+++ Assets/Scripts/Patterns/DecoratorPattern/Potion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : BasePotion
+{
+    public override void Use() {
+        Debug.Log("Potion consumed");
+        Destroy(gameObject);
+    }
+
+    protected void Start () {
+        Debug.Log("Potion initialized");
+    }
+
+    protected void Update() {
+        if (Input.GetKey(KeyCode.E)) {
+            Use();
+        }
+    }
+}
