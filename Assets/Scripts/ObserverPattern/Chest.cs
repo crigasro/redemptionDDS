9ebfac718c273040d683ad017ff3fe103091e5c6
crigasro <gasparrozalen@gmail.com>
2019-06-01 12:23:02 +0200

    Avances patrón observable

---
+++ Assets/Scripts/ObserverPattern/Chest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Subject
+{
+    private string _chestState;
+    public string ChestState {
+        get {return _chestState; }
+        set { _chestState = value; }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            Debug.Log("Jugador frente a cofre");
+        }
+    }
+}
---
+++ Assets/Scripts/ObserverPattern/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Subject : MonoBehaviour
+{
+    private List<Observer> _observers = new List<Observer>();
+
+    public void AttachObserver(Observer observer)
+    {
+      _observers.Add(observer);
+    }
+
+    public void DetachObserver(Observer observer)
+    {
+      _observers.Remove(observer);
+    }
+
+    public void NotifyObserver()
+    {
+      foreach (Observer o in _observers) { o.OnNotify(); }
+    }
+}
