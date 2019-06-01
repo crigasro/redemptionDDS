ccc1fd542d5cb32e673b587fa32763ac501d4fc9
arnaupool <arnyr@hotmail.es>
2019-06-01 18:21:18 +0200

    Map_04 hecho

---
+++ Assets/Scripts/Patterns/DecoratorPattern/FireDecorator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDecorator : PotionDecorator
+{
+    public FireDecorator (Potion p) :  base(p) {
+    }
+
+    public new void Use () {
+        base.Use(); //Utiliza el Use() de PotionDecorator
+        Debug.Log("Use() of FireDecorator");
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            other.gameObject.GetComponent<PlayerController>().giveFire();
+            Use();
+        }
+    }
+}
---
+++ Assets/Scripts/Patterns/DecoratorPattern/PotionDecorator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PotionDecorator : MonoBehaviour, IBasePotion
+{
+    public Potion potion;
+
+    public PotionDecorator (Potion p) {
+        this.potion = p;
+    }
+    public void Use() {
+        this.potion.Use();  //El Use() de Potion
+        Debug.Log("Use() of PotionDecorator");
+    }
+}
