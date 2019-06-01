b5189a7e8d31519da6172bf85724a86c842b1f70
arnaupool <arnyr@hotmail.es>
2019-06-01 02:11:10 +0200

    Avances en la primera poción

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,17 +7,36 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Vector2 moveVelocity;
     private Rigidbody2D rb;
+
+    public Transform firePoint;
+    public GameObject fireball;
+
+    public bool firePower = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+
+        //Hacer muchas refactorizaciones aquí -- Lo estoy haciendo muy guarro
         Vector2 moveInput =  new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
+
+        //Poción de fuego
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            firePower = !firePower;
+            Debug.Log("firePower is: " + firePower);
+        }
+        //Disparación de bola de fuego
+        if (Input.GetKeyDown(KeyCode.Space) && firePower) {
+            Debug.Log("Player position: " + transform.position + "\nFirepoint position: " + firePoint.position);
+            Instantiate(fireball, firePoint.position, firePoint.rotation);
+        }
     }
 
     void FixedUpdate() {
+        //Y puede que aquí
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
