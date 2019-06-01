67a402b98fdf6a877d423a662c133514616f9c35
arnaupool <arnyr@hotmail.es>
2019-06-01 12:19:24 +0200

    Poción 1 hecha

---
+++ Assets/Scripts/Player/FirePotionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePotionScript : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void OnTriggerEnter2D (Collider2D collider) {
+        Debug.Log("Collision by fireball");
+        Destroy(gameObject);
+    }
+}
