27207abf058cf99163c0df05ba994a36aec30694
crigasro <gasparrozalen@gmail.com>
2019-06-03 00:01:08 +0200

    Merge

---
+++ Assets/Scripts/Patterns/ObserverPattern/PotionAchivement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PotionAchivement : Subject
+{
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 3 && GameManager.instance.firePower)
+        {
+            NotifyObserver(NotifType.AchivementUnlocked, true);
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 4 && GameManager.instance.icePower)
+        {
+            NotifyObserver(NotifType.AchivementUnlocked, true);
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 5 && GameManager.instance.lifePower)
+        {
+            NotifyObserver(NotifType.AchivementUnlocked, true);
+        }
+    }
+}
