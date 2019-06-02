f6fba69a2b9222f61c336c3b2ed6fd14debaa9be
crigasro <gasparrozalen@gmail.com>
2019-06-02 18:44:08 +0200

    mis cambios

---
+++ Assets/Scripts/Patterns/ObserverPattern/FirstChest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstChest : Subject
+{
+    private Animator animChest;
+    private bool inChest;
+
+    void Start() {
+        animChest = GetComponent<Animator>();
+    }
+
+    void Update() {
+        OpenChest();
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player") ){
+            inChest = true;
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") ){
+            inChest = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") ){
+            inChest = false;
+        }
+    }
+
+    private void OpenChest() {
+        if(Input.GetKeyDown(KeyCode.E) && inChest) {
+            animChest.SetBool("chestIsOpen", true);
+            //GameManager.instance.GiveRandomObjectFromChest();
+            NotifyObserver(NotifType.OpenedTutorialChest, true);
+        }
+    }
+}
