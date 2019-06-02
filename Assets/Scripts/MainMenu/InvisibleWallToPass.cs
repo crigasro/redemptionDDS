485664d6dda3b42e9bd2d67909d85475a2929926
crigasro <gasparrozalen@gmail.com>
2019-06-02 17:13:09 +0200

    Adaptados los mapas y textos

---
+++ Assets/Scripts/MainMenu/InvisibleWallToPass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InvisibleWallToPass : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Player"))
+        {
+            int index = SceneManager.GetActiveScene().buildIndex;
+            GameManager.instance.LoadNextScenario(index);
+        }
+    }
+}
