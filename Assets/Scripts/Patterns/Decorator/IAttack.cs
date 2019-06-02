fd5423ca448d44517b2f180da015bb07b9829227
FVJohnny <beox12@gmail.com>
2019-06-02 16:59:20 +0200

    Fix fireball + empezando decorador de attack

---
+++ Assets/Scripts/Patterns/Decorator/IAttack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAttack
+{
+    void OnSpawn(GameObject projectile);
+    void OnLand(Collision2D collisionData);
+}
+
+public abstract class AttackDecorator : IAttack
+{
+    private IAttack _attack;
+
+    public AttackDecorator(IAttack attack)
+    {
+        this._attack = attack;
+    }
+
+    public virtual void OnSpawn(GameObject projectile) { }
+    public virtual void OnLand(Collision2D collisionData) { }
+
+}
+
+public class FireAttack : AttackDecorator
+{
+    public FireAttack(IAttack attack) : base(attack)
+    {
+
+    }
+
+    public override void OnSpawn(GameObject projectile)
+    {
+        base.OnSpawn(projectile);
+
+
+    }
+}
