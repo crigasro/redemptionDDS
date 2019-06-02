bf53d73e85aeb9ed8e0077a9f0e16a52f47cf47d
arnaupool <arnyr@hotmail.es>
2019-06-03 01:02:54 +0200

    Toques finales a los mapas y pulido de todo en general

--- Assets/Scripts/ObjectsScript/TempleDoor.cs
+++ Assets/Scripts/ObjectsScript/TempleDoor.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TempleDoor : MonoBehaviour
 {
-    void OnTriggerEnter2D(GameObject other) {
-        if (other.CompareTag("Player"))
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player"))
         {
-            GameManager.instance.LoadNextScenario(6);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
     
