c142817cf39f3bd8170c9d1f03315c1bf29624dc
Anleus <tomas.rui598@gmail.com>
2019-06-02 16:07:29 +0200

    si te caes y eso te mueres

---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private SpawnPoint sp;
+
+    void Start()
+    {
+        sp = FindObjectOfType<SpawnPoint>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) 
+        {
+            other.transform.position = sp.transform.position;
+        }
+    }
+}
