7373df1a26fe4ba5788a23a4d4551c59b6543189
arnaupool <arnyr@hotmail.es>
2019-06-03 15:28:44 +0200

    Cambios

---
+++ Assets/Scripts/MainMenu/EndButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndButton : MonoBehaviour
+{
+    public void ClickDie(){
+        SceneManager.LoadScene(8);
+    }
+
+    public void ClickLive(){
+        SceneManager.LoadScene(9);
+    }
+
+    public void Return() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Continue() {
+        SceneManager.LoadScene(10);
+    }
+}
